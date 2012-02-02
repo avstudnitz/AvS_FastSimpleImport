eb0305c100dfec5fc8f862e805d0fbf7a6b0d8e0
Andreas von Studnitz <avs@avs-webentwicklung.de>
2012-02-02 12:57:21 +0100

    [+FEATURE] Added additional options for category referencing: name starting from base category, or category id

--- src/app/code/community/AvS/FastSimpleImport/Model/ImportEntity/Product.php
+++ src/app/code/community/AvS/FastSimpleImport/Model/ImportEntity/Product.php
@@ -22,4 +22,69 @@ class AvS_FastSimpleImport_Model_ImportEntity_Product extends Mage_ImportExport_
 
         return $this;
     }
+
+    /**
+     * Import behavior setter
+     *
+     * @param string $behavior
+     */
+    public function setBehavior($behavior)
+    {
+        $this->_parameters['behavior'] = $behavior;
+    }
+
+    /**
+     * Returns an object for upload a media files
+     *
+     * @return Mage_ImportExport_Model_Import_Uploader
+     */
+    protected function _getUploader()
+    {
+        if (is_null($this->_fileUploader)) {
+            $this->_fileUploader    = new Mage_ImportExport_Model_Import_Uploader();
+
+            $this->_fileUploader->init();
+
+            $tmpDir     = Mage::getConfig()->getOptions()->getMediaDir() . DS . 'promo_import' . DS . 'images';
+            $destDir    = Mage::getConfig()->getOptions()->getMediaDir() . DS . 'catalog' . DS . 'product';
+            if (!is_writable($destDir)) {
+                @mkdir($destDir, 0777, true);
+            }
+            if (!$this->_fileUploader->setTmpDir($tmpDir)) {
+                Mage::throwException("File directory '{$tmpDir}' is not readable.");
+            }
+            if (!$this->_fileUploader->setDestDir($destDir)) {
+                Mage::throwException("File directory '{$destDir}' is not writable.");
+            }
+        }
+        return $this->_fileUploader;
+    }
+
+    /**
+     * Initialize categories text-path to ID hash.
+     *
+     * @return Mage_ImportExport_Model_Import_Entity_Product
+     */
+    protected function _initCategories()
+    {
+        $collection = Mage::getResourceModel('catalog/category_collection')->addNameToResult();
+        /* @var $collection Mage_Catalog_Model_Resource_Eav_Mysql4_Category_Collection */
+        foreach ($collection as $category) {
+            $structure = explode('/', $category->getPath());
+            $pathSize  = count($structure);
+            if ($pathSize > 2) {
+                $path = array();
+                $this->_categories[implode('/', $path)] = $category->getId();
+                for ($i = 2; $i < $pathSize; $i++) {
+                    $path[] = $collection->getItemById($structure[$i])->getName();
+                }
+                $this->_categories[implode('/', $path)] = $category->getId();
+
+                // additional options for category referencing: name starting from base category, or category id
+                $this->_categories[$collection->getItemById($structure[1])->getName() . '/' . implode('/', $path)] = $category->getId();
+                $this->_categories[$category->getId()] = $category->getId();
+            }
+        }
+        return $this;
+    }
 }
