4182cc9e599f97ae0a714b8e002af6c26522050d
paales <paul@h-o.nl>
2012-12-18 12:31:40 +0100

    Made compatible with the AvS_FastSimpleImport

--- src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/Category.php
+++ src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/Category.php
@@ -482,7 +482,7 @@ class AvS_FastSimpleImport_Model_Import_Entity_Category extends Mage_ImportExpor
 
                 foreach (array_intersect_key($rowData, $this->_attributes) as $attrCode => $attrValue) {
                     if (!$this->_attributes[$attrCode]['is_static'] && strlen($attrValue)) {
-
+                        
                         /** @var $attribute Mage_Eav_Model_Entity_Attribute */
                         $attribute = $this->_attributes[$attrCode]['attribute'];
 
@@ -824,5 +824,93 @@ class AvS_FastSimpleImport_Model_Import_Entity_Category extends Mage_ImportExpor
             }
         }
         return $categoryIds;
+    }
+
+    /**
+     * Check one attribute. Can be overridden in child. Copied this validator
+     * from the customer importer.
+     *
+     * @param string $attrCode Attribute code
+     * @param array $attrParams Attribute params
+     * @param array $rowData Row data
+     * @param int $rowNum
+     * @return boolean
+     */
+    public function isAttributeValid($attrCode, array $attrParams, array $rowData, $rowNum)
+    {
+        $message = '';
+        switch ($attrParams['type']) {
+            case 'varchar':
+                $val   = Mage::helper('core/string')->cleanString($rowData[$attrCode]);
+                $valid = Mage::helper('core/string')->strlen($val) < self::DB_MAX_VARCHAR_LENGTH;
+                $message = 'String is too long, only ' . self::DB_MAX_VARCHAR_LENGTH . ' characters allowed.';
+                break;
+            case 'decimal':
+                $val   = trim($rowData[$attrCode]);
+                $valid = (float)$val == $val;
+                $message = 'Decimal value expected.';
+                break;
+            case 'select':
+            case 'multiselect':
+                $valid = isset($attrParams['options'][strtolower($rowData[$attrCode])]);
+                $message = 'Possible options are: ' . implode(', ', array_keys($attrParams['options']));
+                break;
+            case 'int':
+                $val   = trim($rowData[$attrCode]);
+                $valid = (int)$val == $val;
+                $message = 'Integer value expected.';
+                break;
+            case 'datetime':
+                $val   = trim($rowData[$attrCode]);
+                $valid = strtotime($val) !== false
+                    || preg_match('/^\d{2}.\d{2}.\d{2,4}(?:\s+\d{1,2}.\d{1,2}(?:.\d{1,2})?)?$/', $val);
+                $message = 'Datetime value expected.';
+                break;
+            case 'text':
+                $val   = Mage::helper('core/string')->cleanString($rowData[$attrCode]);
+                $valid = Mage::helper('core/string')->strlen($val) < self::DB_MAX_TEXT_LENGTH;
+                $message = 'String is too long, only ' . self::DB_MAX_TEXT_LENGTH . ' characters allowed.';
+                break;
+            default:
+                $valid = true;
+                break;
+        }
+
+        if (!$valid) {
+            $this->addRowError(Mage::helper('importexport')->__("Invalid value for '%s'") . '. ' . $message, $rowNum, $attrCode);
+        } elseif (!empty($attrParams['is_unique'])) {
+            if (isset($this->_uniqueAttributes[$attrCode][$rowData[$attrCode]])) {
+                $this->addRowError(Mage::helper('importexport')->__("Duplicate Unique Attribute for '%s'"), $rowNum, $attrCode);
+                return false;
+            }
+            $this->_uniqueAttributes[$attrCode][$rowData[$attrCode]] = true;
+        }
+        return (bool) $valid;
+    }
+
+
+    /**
+     * Source model setter.
+     *
+     * @param array $source
+     * @return Mage_ImportExport_Model_Import_Entity_Abstract
+     */
+    public function setArraySource($source)
+    {
+        $this->_source = $source;
+        $this->_dataValidated = false;
+
+        return $this;
+    }
+
+
+    /**
+     * Import behavior setter
+     *
+     * @param string $behavior
+     */
+    public function setBehavior($behavior)
+    {
+        $this->_parameters['behavior'] = $behavior;
     }
 }
