df0d5cb870425cf6762bf9261855a6cb137d4cf6
Andreas von Studnitz <avs@integer-net.de>
2013-01-03 22:58:11 +0100

    [+BUGFIX] Stock data got always replaced by first stock item (fixes #3)

--- src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/Product.php
+++ src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/Product.php
@@ -494,4 +494,94 @@ class AvS_FastSimpleImport_Model_Import_Entity_Product extends Mage_ImportExport
         }
         return (bool) $valid;
     }
+
+    /**
+     * Stock item saving.
+     * Overwritten in order to fix bug with stock data import
+     * See http://www.magentocommerce.com/bug-tracking/issue/?issue=13539
+     * See https://github.com/avstudnitz/AvS_FastSimpleImport/issues/3
+     *
+     * @return Mage_ImportExport_Model_Import_Entity_Product
+     */
+    protected function _saveStockItem()
+    {
+        $defaultStockData = array(
+            'manage_stock'                  => 1,
+            'use_config_manage_stock'       => 1,
+            'qty'                           => 0,
+            'min_qty'                       => 0,
+            'use_config_min_qty'            => 1,
+            'min_sale_qty'                  => 1,
+            'use_config_min_sale_qty'       => 1,
+            'max_sale_qty'                  => 10000,
+            'use_config_max_sale_qty'       => 1,
+            'is_qty_decimal'                => 0,
+            'backorders'                    => 0,
+            'use_config_backorders'         => 1,
+            'notify_stock_qty'              => 1,
+            'use_config_notify_stock_qty'   => 1,
+            'enable_qty_increments'         => 0,
+            'use_config_enable_qty_inc'     => 1,
+            'qty_increments'                => 0,
+            'use_config_qty_increments'     => 1,
+            'is_in_stock'                   => 0,
+            'low_stock_date'                => null,
+            'stock_status_changed_auto'     => 0,
+            'is_decimal_divided'            => 0
+        );
+
+        $entityTable = Mage::getResourceModel('cataloginventory/stock_item')->getMainTable();
+        $helper      = Mage::helper('catalogInventory');
+
+        while ($bunch = $this->_dataSourceModel->getNextBunch()) {
+            $stockData = array();
+
+            // Format bunch to stock data rows
+            foreach ($bunch as $rowNum => $rowData) {
+                if (!$this->isRowAllowedToImport($rowData, $rowNum)) {
+                    continue;
+                }
+                // only SCOPE_DEFAULT can contain stock data
+                if (self::SCOPE_DEFAULT != $this->getRowScope($rowData)) {
+                    continue;
+                }
+
+                $row['product_id'] = $this->_newSku[$rowData[self::COL_SKU]]['entity_id'];
+                $row['stock_id'] = 1;
+
+                /** @var $stockItem Mage_CatalogInventory_Model_Stock_Item */
+                $stockItem = Mage::getModel('cataloginventory/stock_item');
+                $stockItem->loadByProduct($row['product_id']);
+                $existStockData = $stockItem->getData();
+
+                $row = array_merge(
+                    $row,
+                    $defaultStockData,
+                    array_intersect_key($existStockData, $defaultStockData),
+                    array_intersect_key($rowData, $defaultStockData)
+                );
+
+                $stockItem->setData($row);
+
+                if ($helper->isQty($this->_newSku[$rowData[self::COL_SKU]]['type_id'])) {
+                    if ($stockItem->verifyNotification()) {
+                        $stockItem->setLowStockDate(Mage::app()->getLocale()
+                                ->date(null, null, null, false)
+                                ->toString(Varien_Date::DATETIME_INTERNAL_FORMAT)
+                        );
+                    }
+                    $stockItem->setStockStatusChangedAutomatically((int) !$stockItem->verifyStock());
+                } else {
+                    $stockItem->setQty(0);
+                }
+                $stockData[] = $stockItem->unsetOldData()->getData();
+            }
+
+            // Insert rows
+            if ($stockData) {
+                $this->_connection->insertOnDuplicate($entityTable, $stockData);
+            }
+        }
+        return $this;
+    }
 }
