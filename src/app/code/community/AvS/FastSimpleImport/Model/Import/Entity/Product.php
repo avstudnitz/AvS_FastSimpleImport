6036fd31ccbc26be669aaca1d94631101ea1508c
Andreas von Studnitz <avs@integer-net.de>
2013-01-03 22:31:41 +0100

    [~BUGFIX] Corrected check for existing images when using http import (fixes #19)

--- src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/Product.php
+++ src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/Product.php
@@ -145,7 +145,7 @@ class AvS_FastSimpleImport_Model_Import_Entity_Product extends Mage_ImportExport
                 && strpos($rowData['_media_image'], 'http') === 0
                 && strpos($rowData['_media_image'], '://') !== false
             ) {
-                if (!is_file(basename($this->_getUploader()->getTmpDir() . DS . $rowData['_media_image']))) {
+                if (!is_file($this->_getUploader()->getTmpDir() . DS . basename($rowData['_media_image']))) {
                     $this->_copyExternalImageFile($rowData['_media_image']);
                 }
                 $this->_getSource()->setValue('_media_image', basename($rowData['_media_image']));
