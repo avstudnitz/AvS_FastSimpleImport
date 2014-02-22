02f72f65b9ebcd469a5471f2d59f3ed23e14fa21
Richard.Vogel <richard.vogel@iwelt.de>
2014-02-22 11:45:36 +0100

    Added base configurations for EcomDev-Unittests

---
+++ src/app/code/community/AvS/FastSimpleImport/Test/Config/MainTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * Date: 22.02.14
+ * Time: 11:30
+ */
+
+class AvS_FastSimpleImport_Test_Config_MainTest extends EcomDev_PHPUnit_Test_Case_Config
+{
+
+
+    public function testIfTrueIsFalse()
+    {
+        $this->assertTrue(false);
+    }
+}
---
+++ src/app/code/community/AvS/FastSimpleImport/Test/Model/ImportTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * Date: 22.02.14
+ * Time: 11:42
+ */
+
+class AvS_FastSimpleImport_Test_Model_Import extends EcomDev_PHPUnit_Test_Case {
+
+
+    public function testIfTrueIsFalse()
+    {
+        $this->assertTrue(false);
+    }
+}
