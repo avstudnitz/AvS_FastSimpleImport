85b7c33381cc27fe2f42dd7f524041bae4fbb66a
Rokko11 <richardvogel123@gmx.de>
2014-02-22 14:00:34 +0100

    Added virtual product type to Import Types

---
+++ src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/Product/Type/Virtual.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * Date: 22.02.14
+ * Time: 13:57
+ */
+
+class AvS_FastSimpleImport_Model_Import_Entity_Product_Type_Virtual extends AvS_FastSimpleImport_Model_Import_Entity_Product_Type_Simple {
+
+}
