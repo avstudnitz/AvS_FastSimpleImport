25cdcc967aeba11fcbfb5ce37bcfcff05333889c
Andreas von Studnitz <avs@integer-net.de>
2014-02-22 12:40:17 +0100

    [+EXPERIMENTAL] Extend Adapter to support multi arrays

--- src/app/code/community/AvS/FastSimpleImport/Model/ArrayAdapter.php
+++ src/app/code/community/AvS/FastSimpleImport/Model/ArrayAdapter.php
@@ -16,9 +16,24 @@ class AvS_FastSimpleImport_Model_ArrayAdapter implements SeekableIterator
     protected $_position = 0;
 
     /**
+     * @var int
+     */
+    protected $_subPosition = 0;
+
+    /**
+     * @var int
+     */
+    protected $_maxSubPosition = 0;
+
+    /**
      * @var array The Data; Array of Array
      */
     protected $_array = array();
+
+    /**
+     * @var array ; Array of Strings
+     */
+    protected $_subArray = array();
 
     /**
      * Go to given position and check if it is valid
@@ -66,13 +81,26 @@ class AvS_FastSimpleImport_Model_ArrayAdapter implements SeekableIterator
      */
     public function current()
     {
-        return $this->_array[$this->_position];
+        $lineData = $this->_array[$this->_position];
+        
+        if ($this->_useMultiArrays()) {
+            
+            if (!isset($this->_subArray[$this->_position])) {
+
+                $this->_createSubArray($lineData);
+            }
+            
+            print_r($this->_subArray[$this->_position][$this->_subPosition]);
+            return $this->_subArray[$this->_position][$this->_subPosition];
+        }
+        
+        return $lineData;
     }
 
     /**
      * Get current position
      *
-     * @return scalar
+     * @return int
      */
     public function key()
     {
@@ -86,7 +114,14 @@ class AvS_FastSimpleImport_Model_ArrayAdapter implements SeekableIterator
      */
     public function next()
     {
-        ++$this->_position;
+        if ($this->_useMultiArrays()) {
+            ++$this->_subPosition;
+            if ($this->_subPosition > $this->_maxSubPosition) {
+                ++$this->_position;
+            }
+        } else {
+            ++$this->_position;
+        }
     }
 
     /**
@@ -124,5 +159,36 @@ class AvS_FastSimpleImport_Model_ArrayAdapter implements SeekableIterator
         }
 
         $this->_array[$this->_position][$key] = $value;
+    }
+    
+    protected function _useMultiArrays()
+    {
+        return true;
+    }
+
+    /**
+     * @param array $lineData
+     */
+    protected function _createSubArray($lineData)
+    {
+        $this->_subArray = array(
+            $this->_position => array(
+                0 => $lineData,
+            ),
+        );
+
+        foreach ($lineData as $key => $value) {
+            if (is_array($value)) {
+                foreach ($value as $i => $subValue) {
+                    $this->_subArray[$this->_position][$i][$key] = $subValue;
+                    if ($i > 0) {
+                        $this->_subArray[$this->_position][$i]['sku'] = null;
+                    }
+                }
+            }
+        }
+
+        $this->_subPosition = 0;
+        $this->_maxSubPosition = sizeof($this->_subArray[$this->_position]) - 1;
     }
 }
