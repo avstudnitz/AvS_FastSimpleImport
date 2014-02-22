2a20d3de7817096e06905c63ba2294c4f7302970
Rokko11 <richardvogel123@gmx.de>
2014-02-22 14:01:20 +0100

    Added base test of configuration

--- src/app/code/community/AvS/FastSimpleImport/Test/Config/MainTest.php
+++ src/app/code/community/AvS/FastSimpleImport/Test/Config/MainTest.php
@@ -8,9 +8,52 @@
 class AvS_FastSimpleImport_Test_Config_MainTest extends EcomDev_PHPUnit_Test_Case_Config
 {
 
-
-    public function testIfTrueIsFalse()
+    /**
+     * @test
+     */
+    public function testIfCategoryEntityIsLoadedProperly()
     {
-        $this->assertTrue(false);
+        $this->assertModelAlias('fastsimpleimport/import_entity_category', 'AvS_FastSimpleImport_Model_Import_Entity_Category');
     }
+
+    /**
+     * @test
+     */
+    public function testIfSimpleProductEntityIsLoadedProperly()
+    {
+        $this->assertModelAlias('fastsimpleimport/import_entity_product_type_simple', 'AvS_FastSimpleImport_Model_Import_Entity_Product_Type_Simple');
+    }
+
+    /**
+     * @test
+     */
+    public function testIfConfigurableProductEntityIsLoadedProperly()
+    {
+        $this->assertModelAlias('fastsimpleimport/import_entity_product_type_configurable', 'AvS_FastSimpleImport_Model_Import_Entity_Product_Type_Configurable');
+    }
+
+    /**
+     * @test
+     */
+    public function testIfVirtualProductEntityIsLoadedProperly()
+    {
+        $this->assertModelAlias('fastsimpleimport/import_entity_product_type_virtual', 'AvS_FastSimpleImport_Model_Import_Entity_Product_Type_Virtual');
+    }
+
+    /**
+     * @test
+     */
+    public function testIfGroupedProductEntityIsLoadedProperly()
+    {
+        $this->assertModelAlias('fastsimpleimport/import_entity_product_type_Grouped', 'AvS_FastSimpleImport_Model_Import_Entity_Product_Type_Grouped');
+    }
+
+    /**
+     * @test
+     */
+    public function testIfBundleProductEntityIsLoadedProperly()
+    {
+        $this->assertModelAlias('fastsimpleimport/import_entity_product_type_Bundle', 'AvS_FastSimpleImport_Model_Import_Entity_Product_Type_Bundle');
+    }
+
 }
