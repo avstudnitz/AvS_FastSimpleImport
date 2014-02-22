6282c72bfa83ccdd85d18a73800fac853a9dd7db
Thore Baden <tb@tb-TERRA-MOBILE-1512-1712.(none)>
2014-02-22 20:07:02 +0100

    new createOrUpdateAttributeValue

--- src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/AttributeOptions.php
+++ src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/AttributeOptions.php
@@ -12,13 +12,49 @@ class AvS_FastSimpleImport_Model_Import_Entity_AttributeOptions
     const COL_DELETE = 'delete';
 
 
-    public function createAttributeValue($attCode, $attValue) {
+    public function createOrUpdateAttributeValue($attCode, $attValue) {
         $attribute_code=Mage::getModel('eav/entity_attribute')->getIdByCode('catalog_product', $attCode);
         $attributeInfo = Mage::getModel('eav/entity_attribute')->load($attribute_code);
         $attribute_table = Mage::getModel('eav/entity_attribute_source_table')->setAttribute($attributeInfo);
+        $aopt = $attribute_table->getAllOptions(false);
         $opt = $attValue;
-
         $option = array(self::COL_VALUE => array(), self::COL_ORDER => array(), self::COL_DELETE => array());
+        if (is_array($attValue)) {
+            foreach ($aopt as $ao) {
+                if ($ao['label'] == $attValue[0]) {
+                    $i = 0;
+                    foreach ($attValue as $aV) {
+                        $option[self::COL_VALUE][$ao['value']][$i] = $aV;
+                        $i++;
+                    }
+                    $attributeInfo->setOption($option);
+                    $attributeInfo->save();
+                    return;
+                }
+            }
+        } else {
+            foreach ($aopt as $ao) {
+                if ($ao['label'] == $attValue) {
+                    $i = 0;
+                    $option[self::COL_VALUE][$ao['value']][0] = $attValue;
+                    $attributeInfo->setOption($option);
+                    $attributeInfo->save();
+                    return;
+                }
+            }
+        }
+
+
+        foreach ($aopt as $ao) {
+            if ($ao['label'] == $attValue[0]) {
+                $option[self::COL_VALUE][$ao['value']][0] = $attValue[0];
+                $option[self::COL_VALUE][$ao['value']][1] = $attValue[1];
+                print_r($option);
+                $attributeInfo->setOption($option);
+                $attributeInfo->save();
+                return;
+            }
+        }
         if (is_array($opt)) {
             $i = 0;
             foreach ($opt as $o) {
