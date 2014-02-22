24ba1e20ba9e3e50c851ae51fd345cf3bb5f67db
Rokko11 <richardvogel123@gmx.de>
2014-02-22 16:13:03 +0100

    Tested DefaultValues of general options

--- src/app/code/community/AvS/FastSimpleImport/Test/Config/DefaultValueTest.php
+++ src/app/code/community/AvS/FastSimpleImport/Test/Config/DefaultValueTest.php
@@ -13,5 +13,11 @@ class AvS_FastSimpleImport_Test_Config_DefaultValueTest extends EcomDev_PHPUnit_
         $this->assertDefaultConfigValue('fastsimpleimport/product/status', 1);
         $this->assertDefaultConfigValue('fastsimpleimport/product/visibility', 4);
         $this->assertDefaultConfigValue('fastsimpleimport/product/weight', 0);
+
+        $this->assertDefaultConfigValue('fastsimpleimport/general/import_behavior', 'replace');
+        $this->assertDefaultConfigValue('fastsimpleimport/general/partial_indexing', 0);
+        $this->assertDefaultConfigValue('fastsimpleimport/general/continue_after_errors', 0);
+        $this->assertDefaultConfigValue('fastsimpleimport/general/error_limit', 100);
+        $this->assertDefaultConfigValue('fastsimpleimport/general/support_nested_arrays', 0);
     }
 }
