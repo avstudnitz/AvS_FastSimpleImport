ac468d7e82b1f71cd127f61f4a4bcc433b8a2a2a
Rokko11 <richardvogel123@gmx.de>
2014-02-23 12:49:11 +0100

    Added configurable Test: Create two simple products and one configurable.

--- src/app/code/community/AvS/FastSimpleImport/Test/Model/Product/ConfigurableTest.php
+++ src/app/code/community/AvS/FastSimpleImport/Test/Model/Product/ConfigurableTest.php
@@ -2,79 +2,58 @@
 
 class AvS_FastSimpleImport_Test_Model_Product_ConfigurableTest extends EcomDev_PHPUnit_Test_Case
 {
+    protected function setUp()
+    {
+        Mage::getSingleton('core/resource')->getConnection('core_write')
+        ->query('delete from catalog_product_entity');
+
+        $model = Mage::getModel('eav/entity_setup', 'core_setup');
+        $attributeId = $model->getAttribute('catalog_product', 'color');
+        $attributeSetId = $model->getDefaultAttributeSetId('catalog_product');
+        //Get attribute group info
+        $attributeGroupId = $model->getAttributeGroup('catalog_product', $attributeSetId, 'General');
+        //add attribute to a set
+        $model->addAttributeToSet('catalog_product', $attributeSetId, $attributeGroupId['attribute_group_id'], $attributeId['attribute_id']);
+
+        parent::setUp();
+    }
+
 
     /**
      * @test
      * @loadExpectation
-     * @loadFixture simpleProducts.yaml
      * @dataProvider dataProvider
      */
     public function createProduct($values)
     {
-        Mage::getModel('fastsimpleimport/import')->processProductImport($values);
+        $productBaseModel = Mage::getModel('catalog/product');
 
-        $sku = (string) $values[0]['sku'];
-        $product = Mage::getModel('catalog/product');
-        $product->load($product->getIdBySku($sku));
-        $expected = $this->expected('%s-%s', $sku, 1);
+        $this->assertCount(0, $productBaseModel->getCollection()->getAllIds());
+        Mage::getModel('fastsimpleimport/import')->setDropdownAttributes(array('color'))->processProductImport($values);
 
-        foreach ($expected as $key => $value) {
-            $this->assertEquals($value, $product->getData($key));
+        $this->assertCount(3, $productBaseModel->getCollection()->getAllIds());
+
+        $configurableProducts = $productBaseModel->getCollection()->addFieldToFilter('type_id' ,array('eq' => 'configurable'))->load();
+        $this->assertCount(1, $configurableProducts);
+
+        $sku = $configurableProducts->getFirstItem()->getSku();
+
+        $code = $this->expected('%s-%s', $sku, 'children')->getCodes();
+        $childrenSkus = $this->expected('%s-%s', $sku, 'children')->getSkus();
+        /** @var Mage_Catalog_Model_Product_Type_Configurable $configurable */
+        $productId = Mage::getModel('catalog/product')->getIdBySku($sku);
+        $productModel = Mage::getModel('catalog/product')->load($productId);
+        $configurable = $productModel->getTypeInstance(true);
+        $configurableOptions = $configurable->getConfigurableAttributesAsArray($productModel);
+        foreach ($configurableOptions as $option) {
+            $this->assertTrue(in_array($option['attribute_code'], $code));
         }
 
-        $stockExpectedItem = $this->expected('%s-%s', $sku, 'stock');
-        $stock = $product->getStockItem();
-        foreach ($stockExpectedItem as $key => $value) {
-            $this->assertEquals($value, $stock->getData($key), null, 0);
+        $usedProducts = $configurable->getUsedProducts(null, $productModel);
+        foreach ($usedProducts as $product) {
+            $this->assertTrue(in_array($product->getSku(), $childrenSkus));
         }
-    }
 
-    /**
-     * @test
-     * @loadExpectation
-     * @loadFixture defaultValues.yaml
-     * @dataProvider dataProvider
-     */
-    public function createProductWithDefault($values)
-    {
-        $this->assertEquals(2, Mage::getStoreConfig('fastsimpleimport/product/status'));
-        $this->assertEquals(4, Mage::getStoreConfig('fastsimpleimport/product/tax_class_id'));
-        $this->assertEquals(3, Mage::getStoreConfig('fastsimpleimport/product/visibility'));
-        $this->assertEquals(12345, Mage::getStoreConfig('fastsimpleimport/product/weight'));
-        Mage::getModel('fastsimpleimport/import')->processProductImport($values);
-        $sku = (string) $values[0]['sku'];
-        $product = Mage::getModel('catalog/product');
-        $product->load($product->getIdBySku($sku));
-        $expected = $this->expected('%s-%s', $sku, 1);
-
-        foreach ($expected as $key => $value) {
-            $this->assertEquals($value, $product->getData($key));
-        }
-    }
-
-
-    /**
-     * @test
-     * @loadExpectation
-     * @dataProvider dataProvider
-     */
-    public function updateProduct($values)
-    {
-        $origData = $values[0];
-        $sku = (string) $values[0]['sku'];
-        Mage::getModel('fastsimpleimport/import')->processProductImport(array($origData));
-
-        $updateData = $values[1];
-        Mage::getModel('fastsimpleimport/import')->processProductImport(array($updateData));
-
-        $product = Mage::getModel('catalog/product');
-        $product->load($product->getIdBySku($sku));
-        $afterCreate = $this->expected('%s-%s', $sku, 'create');
-        $afterUpdate = $this->expected('%s-%s', $sku, 'update');
-        $afterMerge = array_merge($afterCreate->getData(),$afterUpdate->getData());
-        foreach ($afterMerge as $key => $value) {
-            $this->assertEquals($value, $product->getData($key));
-        }
 
     }
 }
