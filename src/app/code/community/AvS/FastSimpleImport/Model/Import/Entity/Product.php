bcb5dcbf851e10c271a3ec468690d8179142f9cc
Paul Hachmang <paul@paul.local>
2014-03-04 14:26:04 +0100

    [+FEAT] Add exception logging when uploading of media files fails.

--- src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/Product.php
+++ src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/Product.php
@@ -835,6 +835,26 @@ class AvS_FastSimpleImport_Model_Import_Entity_Product extends Mage_ImportExport
         }
     }
 
+
+    /**
+     * Uploading files into the "catalog/product" media folder.
+     * Return a new file name if the same file is already exists.
+     *
+     * @param string $fileName
+     * @return string
+     */
+    protected function _uploadMediaFiles($fileName)
+    {
+        try {
+            $res = $this->_getUploader()->move($fileName);
+            return $res['file'];
+        } catch (Exception $e) {
+            Mage::logException($e);
+            return '';
+        }
+    }
+
+
     /**
      * Save product media gallery.
      * Overwritten in order to provide default value for media_attribute_id
