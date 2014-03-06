8b97bc4ecc5683c522d3d1b2f78d5541a77c37b6
Andreas von Studnitz <avs@integer-net.de>
2014-03-06 12:14:10 +0100

    [~BUGFIX] Improved handling of empty config values for image attributes and (multi)select attributes (fixes #107)

--- src/app/code/community/AvS/FastSimpleImport/Model/Import.php
+++ src/app/code/community/AvS/FastSimpleImport/Model/Import.php
@@ -32,13 +32,13 @@ class AvS_FastSimpleImport_Model_Import extends Mage_ImportExport_Model_Import
         $this->setBehavior(Mage::getStoreConfig('fastsimpleimport/general/import_behavior'));
         $this->setPartialIndexing(Mage::getStoreConfigFlag('fastsimpleimport/general/partial_indexing'));
         $this->setContinueAfterErrors(Mage::getStoreConfigFlag('fastsimpleimport/general/continue_after_errors'));
-        $this->setErrorLimit(Mage::getStoreConfig('fastsimpleimport/general/error_limit'));
+        $this->setErrorLimit(intval(Mage::getStoreConfig('fastsimpleimport/general/error_limit')));
         $this->setUseNestedArrays(Mage::getStoreConfigFlag('fastsimpleimport/general/support_nested_arrays'));
         $this->setIgnoreDuplicates(Mage::getStoreConfigFlag('fastsimpleimport/general/ignore_duplicates'));
-        $this->setDropdownAttributes(explode(',', Mage::getStoreConfig('fastsimpleimport/product/select_attributes')));
-        $this->setMultiselectAttributes(explode(',', Mage::getStoreConfig('fastsimpleimport/product/multiselect_attributes')));
+        $this->setDropdownAttributes(array_filter(explode(',', Mage::getStoreConfig('fastsimpleimport/product/select_attributes'))));
+        $this->setMultiselectAttributes(array_filter(explode(',', Mage::getStoreConfig('fastsimpleimport/product/multiselect_attributes'))));
         $this->setAllowRenameFiles(Mage::getStoreConfigFlag('fastsimpleimport/product/allow_rename_files'));
-        $this->setImageAttributes(explode(',', Mage::getStoreConfig('fastsimpleimport/product/additional_image_attributes')));
+        $this->setImageAttributes(array_filter(explode(',', Mage::getStoreConfig('fastsimpleimport/product/additional_image_attributes'))));
     }
 
     /**
