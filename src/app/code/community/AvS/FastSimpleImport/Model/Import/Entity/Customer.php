6983bfd186b0dd17daa5fcc38c0508b46f1a7af4
Paul Hachmang <paul@h-o.nl>
2014-05-09 11:11:10 +0200

    Copied saveData methods to Configurable and Grouped. Copied _saveCustomers method for customers

--- src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/Customer.php
+++ src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/Customer.php
@@ -74,6 +74,93 @@ class AvS_FastSimpleImport_Model_Import_Entity_Customer extends Mage_ImportExpor
     }
 
     /**
+     * Gather and save information about customer entities.
+     *
+     * @return Mage_ImportExport_Model_Import_Entity_Customer
+     */
+    protected function _saveCustomers()
+    {
+        /** @var $resource Mage_Customer_Model_Customer */
+        $resource       = Mage::getModel('customer/customer');
+        $strftimeFormat = Varien_Date::convertZendToStrftime(Varien_Date::DATETIME_INTERNAL_FORMAT, true, true);
+        $table = $resource->getResource()->getEntityTable();
+        $nextEntityId   = Mage::getResourceHelper('importexport')->getNextAutoincrement($table);
+        $passId         = $resource->getAttribute('password_hash')->getId();
+        $passTable      = $resource->getAttribute('password_hash')->getBackend()->getTable();
+
+        while ($bunch = $this->_dataSourceModel->getNextBunch()) {
+            $entityRowsIn = array();
+            $entityRowsUp = array();
+            $attributes   = array();
+
+            $oldCustomersToLower = array_change_key_case($this->_oldCustomers, CASE_LOWER);
+
+            foreach ($bunch as $rowNum => $rowData) {
+                if (!$this->validateRow($rowData, $rowNum)) {
+                    continue;
+                }
+                if (self::SCOPE_DEFAULT == $this->getRowScope($rowData)) {
+                    // entity table data
+                    $entityRow = array(
+                        'group_id'   => empty($rowData['group_id']) ? self::DEFAULT_GROUP_ID : $rowData['group_id'],
+                        'store_id'   => empty($rowData[self::COL_STORE])
+                                        ? 0 : $this->_storeCodeToId[$rowData[self::COL_STORE]],
+                        'created_at' => empty($rowData['created_at'])
+                                        ? now() : gmstrftime($strftimeFormat, strtotime($rowData['created_at'])),
+                        'updated_at' => now()
+                    );
+
+                    $emailToLower = strtolower($rowData[self::COL_EMAIL]);
+                    if (isset($oldCustomersToLower[$emailToLower][$rowData[self::COL_WEBSITE]])) { // edit
+                        $entityId = $oldCustomersToLower[$emailToLower][$rowData[self::COL_WEBSITE]];
+                        $entityRow['entity_id'] = $entityId;
+                        $entityRowsUp[] = $entityRow;
+                    } else { // create
+                        $entityId                      = $nextEntityId++;
+                        $entityRow['entity_id']        = $entityId;
+                        $entityRow['entity_type_id']   = $this->_entityTypeId;
+                        $entityRow['attribute_set_id'] = 0;
+                        $entityRow['website_id']       = $this->_websiteCodeToId[$rowData[self::COL_WEBSITE]];
+                        $entityRow['email']            = $rowData[self::COL_EMAIL];
+                        $entityRow['is_active']        = 1;
+                        $entityRowsIn[]                = $entityRow;
+
+                        $this->_newCustomers[$rowData[self::COL_EMAIL]][$rowData[self::COL_WEBSITE]] = $entityId;
+                    }
+                    // attribute values
+                    foreach (array_intersect_key($rowData, $this->_attributes) as $attrCode => $value) {
+                        if (!$this->_attributes[$attrCode]['is_static'] && strlen($value)) {
+                            /** @var $attribute Mage_Customer_Model_Attribute */
+                            $attribute  = $resource->getAttribute($attrCode);
+                            $backModel  = $attribute->getBackendModel();
+                            $attrParams = $this->_attributes[$attrCode];
+
+                            if ('select' == $attrParams['type']) {
+                                $value = $attrParams['options'][strtolower($value)];
+                            } elseif ('datetime' == $attrParams['type']) {
+                                $value = gmstrftime($strftimeFormat, strtotime($value));
+                            } elseif ($backModel) {
+                                $attribute->getBackend()->beforeSave($resource->setData($attrCode, $value));
+                                $value = $resource->getData($attrCode);
+                            }
+                            $attributes[$attribute->getBackend()->getTable()][$entityId][$attrParams['id']] = $value;
+
+                            // restore 'backend_model' to avoid default setting
+                            $attribute->setBackendModel($backModel);
+                        }
+                    }
+                    // password change/set
+                    if (isset($rowData['password']) && strlen($rowData['password'])) {
+                        $attributes[$passTable][$entityId][$passId] = $resource->hashPassword($rowData['password']);
+                    }
+                }
+            }
+            $this->_saveCustomerEntity($entityRowsIn, $entityRowsUp)->_saveCustomerAttributes($attributes);
+        }
+        return $this;
+    }
+
+    /**
      * Check one attribute. Can be overridden in child.
      *
      * @param string $attrCode Attribute code
