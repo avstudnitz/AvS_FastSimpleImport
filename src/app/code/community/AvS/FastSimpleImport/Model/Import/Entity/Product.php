580ceaf0aecfd7d05c7fb8276a9cfd5c3d871ba4
Paul Hachmang <paul@h-o.nl>
2014-05-26 20:18:22 +0200

    Fix broken category import

--- src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/Product.php
+++ src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/Product.php
@@ -864,7 +864,7 @@ class AvS_FastSimpleImport_Model_Import_Entity_Product extends Mage_ImportExport
                     $categories[$rowSku][$categoryId] = true;
                 } elseif (!empty($categoryPath)) {
                     $categories[$rowSku][$this->_categories[$categoryPath]] = true;
-                } else {
+                } elseif (isset($rowData[self::COL_CATEGORY])) {
                     $categories[$rowSku] = array();
                 }
 
