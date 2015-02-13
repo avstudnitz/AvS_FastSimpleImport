95120081bf8002c3359af62e755228e1608acfed
Jonathan Zahedieh <jonathan.zahedieh@tryzens.com>
2015-02-13 11:43:20 +0000

    fixes #154 - Don't validate "required" catelog attributes

--- src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/Category.php
+++ src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/Category.php
@@ -910,6 +910,9 @@ class AvS_FastSimpleImport_Model_Import_Entity_Category extends Mage_ImportExpor
             foreach ($this->_attributes as $attrCode => $attrParams) {
                 if (isset($rowData[$attrCode]) && strlen($rowData[$attrCode])) {
                     $this->isAttributeValid($attrCode, $attrParams, $rowData, $rowNum);
+                } elseif ($attrParams['is_required'] && in_array($attrCode, $this->_indexValueAttributes)) {
+                    // empty value is allowed for these "required attributes" using the tickbox
+                    continue;
                 } elseif ($attrParams['is_required'] && !isset($this->_categoriesWithRoots[$root][$category])) {
                     $this->addRowError(self::ERROR_VALUE_IS_REQUIRED, $rowNum, $attrCode);
                 }
