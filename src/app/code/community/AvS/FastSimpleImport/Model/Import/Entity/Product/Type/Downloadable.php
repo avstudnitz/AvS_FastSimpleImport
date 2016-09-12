e7b44d4a7c13a9053f628ae62294f284d7aca5f3
Elias Kotlyar <elias.kotlyar@mediawave.de>
2016-09-12 17:51:26 +0200

    [Downloadable] Added the possibility for importing downloadable Products without any options selected

---
+++ src/app/code/community/AvS/FastSimpleImport/Model/Import/Entity/Product/Type/Downloadable.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Class AvS_FastSimpleImport_Model_Import_Entity_Product_Type_Downloadable
+ */
+class AvS_FastSimpleImport_Model_Import_Entity_Product_Type_Downloadable
+    extends AvS_FastSimpleImport_Model_Import_Entity_Product_Type_Simple
+{
+
+}
